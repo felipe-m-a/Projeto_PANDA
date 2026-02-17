3753a45f2856c0f4e530d97f22c159d231006f69
felipe-m-a <127856541+felipe-m-a@users.noreply.github.com>
2026-02-17 12:07:03 -0300

    Inicialização (#2)

---
+++ Assets/Project/Scripts/SceneManagementSystem/EditorInitializationLoader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Project.Scripts.SceneManagementSystem
+{
+    public class EditorInitializationLoader : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        private void Awake()
+        {
+            for (var i = 0; i < SceneManager.loadedSceneCount; i++)
+                if (SceneManager.GetSceneAt(i).path == GameScene.Initialization.Path)
+                    return;
+
+            SceneManager.LoadSceneAsync(GameScene.Initialization.Path, LoadSceneMode.Additive);
+        }
+#endif
+    }
+}
---
+++ Assets/Project/Scripts/SceneManagementSystem/SceneController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Project.Scripts.SceneManagementSystem
+{
+    public class SceneController : MonoBehaviour
+    {
+        private readonly Dictionary<GameScene.SceneType, string> _loadedScenes = new();
+        private bool _isBusy;
+
+        public Coroutine ExecutePlan(SceneTransitionPlan plan)
+        {
+            if (_isBusy)
+            {
+                Debug.LogWarning("Uma transição de cena já está em curso");
+                return null;
+            }
+
+            _isBusy = true;
+            return StartCoroutine(ExecutePlanRoutine(plan));
+        }
+
+        private IEnumerator ExecutePlanRoutine(SceneTransitionPlan plan)
+        {
+            foreach (var sceneType in plan.SceneTypesToUnload)
+                if (_loadedScenes.Remove(sceneType, out var scenePath))
+                    yield return SceneManager.UnloadSceneAsync(scenePath);
+
+            foreach (var (type, path) in plan.ScenesToLoad)
+            {
+                _loadedScenes.Add(type, path);
+                yield return SceneManager.LoadSceneAsync(path, LoadSceneMode.Additive);
+
+                if (path != plan.SceneToActivate) continue;
+                var scene = SceneManager.GetSceneByPath(path);
+                if (scene.IsValid() && scene.isLoaded)
+                    SceneManager.SetActiveScene(scene);
+            }
+
+            _isBusy = false;
+        }
+
+        #region Singleton
+
+        public static SceneController Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+                Destroy(gameObject);
+            else
+                Instance = this;
+        }
+
+        #endregion
+    }
+}
